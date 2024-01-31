75e1edb25e10a365b411dbf02d74c784540bc913
Xavasbekdeveloper <wolfcomp6@gmail.com>
2024-01-31 22:00:06 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// navbar-shrink
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 0) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
+
+// backtop
+
+window.addEventListener("scroll", function () {
+  backtop();
+});
+
+let backTop = document.getElementById("backtop");
+
+function backtop() {
+    if (scrollY > 200) {
+        backTop.classList.add("show-backtop");
+    } else {
+        backTop.classList.remove("show-backtop");
+    }
+}
+
+
+// nav-list-shrink 
+
+let navList = document.getElementById("nav-list");
+let barBtn = document.getElementById("hamburger__btn");
+
+barBtn.addEventListener("click", function () {
+    navList.classList.toggle("show-nav-list")
+})
